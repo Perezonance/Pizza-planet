b5942af826a0dda95189fb0e6aca416ebc7d08f4
Manuel Alex Perez <manuel.parez@toyota.com>
2020-04-22 16:30:36 -0400

    working basic local server

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-//This is a JS file
+const express = require('express');
+
+const app = express();
+
+const port = process.env.PORT || 8080;
+
+app.use(express.json());
+
+
+app.get('/api/pos/', (req, res) => {
+    console.log('Received get request for new order');
+    res.status(200).send('Received new order');
+});
+
+app.listen(port, () => {
+    console.log('Listening on port:' + port);
+});
